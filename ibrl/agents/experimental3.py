293b51293cc36e701969b34993f4f95a0554bdc7
Allegra Latimer <allegralatimer@gmail.com>
2026-03-27 11:29:25 -0700

    Update PR review comments

--- ibrl/agents/experimental3.py
+++ ibrl/agents/experimental3.py
@@ -36,8 +36,9 @@ class ExperimentalAgent3(QLearningAgent):
         probabilities[1] = 1 - probabilities[0]
         return probabilities
 
-    def update(self, probabilities, action, outcome):
-        super().update(None, self.proto_action, outcome)
+    def update(self, probabilities : NDArray[np.float64], action : int, outcome):
+        super().update(self.proto_probabilities, self.proto_action, outcome)
+
 
     def dump_state(self):
         return f"{super().dump_state()}, proto_action: {self.proto_action}"
