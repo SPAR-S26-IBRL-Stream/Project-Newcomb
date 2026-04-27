8b1f9ba03d2a3e024f7a7a87ce7b764af6cc1a1c
Florian Lorkowski <florian@lorkow.ski>
2026-04-27 11:47:12 +0200

    Make internal state of world model more explicit

--- ibrl/infrabayesian/__init__.py
+++ ibrl/infrabayesian/__init__.py
@@ -1,10 +1,11 @@
 from .a_measure import AMeasure
 from .infradistribution import Infradistribution
-from .world_model import WorldModel, MultiBernoulliWorldModel
+from .world_model import WorldModel
+from .world_models.bernoulli_world_model import MultiBernoulliWorldModel
 
 __all__ = [
     "AMeasure",
     "Infradistribution",
     "WorldModel",
-    "MultiBernoulliWorldModel",
+    "MultBernoulliWorldModel",
 ]
