5e7cf6485bdb27a8ee525611e275439a954a98db
Florian Lorkowski <florian@lorkow.ski>
2026-04-27 10:33:29 +0200

    Remove outdated belief object

--- ibrl/infrabayesian/__init__.py
+++ ibrl/infrabayesian/__init__.py
@@ -1,13 +1,8 @@
-from .beliefs import BaseBelief, BernoulliBelief, GaussianBelief, NewcombLikeBelief
 from .a_measure import AMeasure
 from .infradistribution import Infradistribution
 from .world_model import WorldModel, MultiBernoulliWorldModel
 
 __all__ = [
-    "BaseBelief",
-    "BernoulliBelief",
-    "GaussianBelief",
-    "NewcombLikeBelief",
     "AMeasure",
     "Infradistribution",
     "WorldModel",
