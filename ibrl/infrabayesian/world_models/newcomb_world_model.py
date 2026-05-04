86ae02f10d9f75ae3c02c558ff25c936222be35e
Florian Lorkowski <florian@lorkow.ski>
2026-05-04 14:35:20 +0200

    Add Newcomb world model mixtures, learning of predictor accuracy

--- ibrl/infrabayesian/world_models/newcomb_world_model.py
+++ ibrl/infrabayesian/world_models/newcomb_world_model.py
@@ -11,17 +11,26 @@ from ...outcome import Outcome
 class NewcombWorldModelParameters:
     """
     Parameters of Newcomb world model: predictor accuracy
-        predictor_accuracy=1.0  predict exactly according to agent's policy
-        predictor_accuracy=0.5  predict at random (independent of policy)
+        accuracy = 1.0  predict exactly according to agent's policy
+        accuracy = 0.5  predict at random (independent of policy)
+
+    A mixture accuracies:
+        coefficients[i]  Mixing coefficient of i-th component
+        log_accuracy[i]  [log(1-acc),log(acc)] under i-th component
     """
-    predictor_accuracy : float
+    coefficients : np.ndarray  # shape (num_components,)
+    log_accuracy : np.ndarray  # shape (num_components,2)
+
 
 @dataclass
 class NewcombWorldModelBeliefState:
     """
-    Belief is stateless
+    Belief state of Newcomb world model: Histogram of previous observations
+        history[0]  Number of times predictor was wrong
+        history[1]  Number of times predictor was right
     """
-    pass
+    history : np.ndarray  # integer array shape (2,)
+
 
 # Outcomes: {0, A, B, A+B} with small box A, large box B
 # Actions: {1, 2} for one/two box
@@ -49,18 +58,23 @@ class NewcombWorldModel(WorldModel):
             ])
         else:
             self.reward_matrix = reward_matrix
-        
+
         self.num_actions = self.reward_matrix.shape[0]
         assert self.reward_matrix.shape == (self.num_actions, self.num_actions)
 
     def make_params(self, predictor_accuracy=1) -> NewcombWorldModelParameters:
         assert 0.5 <= predictor_accuracy <= 1.0
-        return NewcombWorldModelParameters(float(predictor_accuracy))
+        return NewcombWorldModelParameters(coefficients=np.array([1.]),
+            log_accuracy=np.log(np.maximum(np.array([[1-predictor_accuracy,predictor_accuracy]]),1e-300)))
 
     def mix_params(self, params_list: list, coefficients: np.ndarray):
-        # TODO: Not yet sure how to mix
-        assert len(params_list) == 1
-        return params_list[0]
+        assert coefficients.shape == (len(params_list),)
+        # List of possible outcome distributions is concatenation of distributions from all components
+        log_accuracy = np.concatenate([p.log_accuracy for p in params_list], axis=0)
+        # Mixture coefficients are product of old and new mixture coefficients
+        mixed_coefficients = np.concatenate([p.coefficients * c for p, c in zip(params_list, coefficients)])
+        mixed_coefficients /= mixed_coefficients.sum()  # For numerics
+        return NewcombWorldModelParameters(coefficients=mixed_coefficients, log_accuracy=log_accuracy)
 
     def event_index(self, outcome: Outcome) -> int:
         indices = []
@@ -75,16 +89,26 @@ class NewcombWorldModel(WorldModel):
         return indices[0]
 
     def initial_state(self):
-        return NewcombWorldModelBeliefState()
+        return NewcombWorldModelBeliefState(history=np.zeros(2, dtype=np.int64))
 
-    def update_state(self, state,
+    def update_state(self,
+            state : NewcombWorldModelBeliefState,
             outcome : Outcome,
             action : int,
             policy : np.ndarray):
-        return state
+        new_state = NewcombWorldModelBeliefState(state.history.copy())
+        if np.isclose(policy[action], 1):  # Update only on pure strategies
+            # Updating on mixed strategies would be more complicated, as the
+            # amount of information gained depends on the policy, e.g. uniform
+            # policy -> no information gain because perfect and random
+            # predictor behave identical.
+            # Also, even a random predictor will be correct sometimes. This is
+            # currently not taken into account.
+            new_state.history[int(outcome.observation == action)] += 1
+        return new_state
 
-    def is_initial(self, state) -> bool:
-        return True
+    def is_initial(self, state : NewcombWorldModelBeliefState) -> bool:
+        return state.history[0] == state.history[1] == 0
 
     def compute_likelihood(self, belief_state,
             outcome : Outcome,
@@ -101,12 +125,8 @@ class NewcombWorldModel(WorldModel):
         # j: selected action
         i,j = event // self.num_actions, event % self.num_actions
 
-        perfect_prediction = policy
-        random_prediction = np.ones_like(policy) / len(policy)
-        prediction = perfect_prediction * (2*params.predictor_accuracy - 1) \
-                    + random_prediction * (2 - 2*params.predictor_accuracy)
-
         # Likelihood is just probability of prediction because the action is fixed
+        prediction = self._prediction(belief_state, params, policy)
         return prediction[i]
 
     def compute_expected_reward(self, belief_state,
@@ -118,10 +138,7 @@ class NewcombWorldModel(WorldModel):
         E[reward_function(outcome) | belief_state, params, action].
         Returns a scalar.
         """
-        perfect_prediction = policy
-        random_prediction = np.ones_like(policy) / len(policy)
-        prediction = perfect_prediction * (2*params.predictor_accuracy - 1) \
-                    + random_prediction * (2 - 2*params.predictor_accuracy)
+        prediction = self._prediction(belief_state, params, policy)
         reward_matrix = np.reshape(reward_function, (self.num_actions,self.num_actions))
         rewards = prediction @ reward_matrix
         return rewards[action]
@@ -149,3 +166,25 @@ class NewcombWorldModel(WorldModel):
 
     def to_str(self, params):
         return "[Newcomb]"
+
+    def _prediction(self,
+            state : NewcombWorldModelBeliefState,
+            params : NewcombWorldModelParameters,
+            policy : np.ndarray) -> np.ndarray:
+        """
+        Compute/estimate the prediction that the predictor will make
+        """
+
+        # Estimate predictor accuracy based hypotheses and observations
+        log_likelihood = (params.log_accuracy @ state.history)
+        log_likelihood -= log_likelihood.max()  # For numerical stability
+        probs = params.coefficients @ np.exp(np.expand_dims(log_likelihood,axis=1) + params.log_accuracy)
+        probs /= probs.sum()
+        acc = np.clip(probs[1], 0.5, 1.0)
+
+        # Assemble prediction
+        perfect_prediction = policy
+        random_prediction = np.ones_like(policy) / len(policy)
+        prediction = perfect_prediction * (2*acc - 1) \
+                    + random_prediction * (2 - 2*acc)
+        return prediction
